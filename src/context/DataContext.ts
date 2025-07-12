c8f9f53359b21088416ce7d352bc6b5d323b4775
YaredAlex <yareda25@gmail.com>
2025-07-12 22:42:18 +0300

    books, memeber and loan states

---
+++ src/context/DataContext.ts
@@ -0,0 +1,12 @@
+import { createContext, useContext } from "react";
+import type { DataContextType } from "../types";
+
+export const DataContext = createContext<DataContextType | null>(null);
+
+export const useData = () => {
+  const context = useContext(DataContext);
+  if (!context) {
+    throw new Error('useData must be used within a DataProvider');
+  }
+  return context;
+};
