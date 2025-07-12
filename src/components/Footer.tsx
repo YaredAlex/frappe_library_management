0b29c101f02788d6eeb812be4afa2277561419ad
YaredAlex <yareda25@gmail.com>
2025-07-12 22:37:57 +0300

    Footer compontent

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => (
+  <footer className="bg-gray-800 text-white p-4 text-center mt-8 rounded-t-xl">
+    <div className="container mx-auto">
+      <p>&copy; 2025 Library Management System. All rights reserved.</p>
+    </div>
+  </footer>
+);
