febf5c70d8a54402cdc13bb73c0d2d24f1fa383a
YaredAlex <yareda25@gmail.com>
2025-07-13 23:16:07 +0300

    filtering user based on role==member

--- library_management/api/member.py
+++ library_management/api/member.py
@@ -3,4 +3,20 @@ from frappe import _
 
 @frappe.whitelist()
 def get_members():
-    return frappe.get_all("Member", fields=["name", "full_name", "email", "phone","membership_id"])
+    users = frappe.db.sql("""
+        SELECT
+            u.name,
+            u.first_name,
+            u.last_name,
+            u.email,
+            u.phone
+        FROM
+            `tabUser` u
+        INNER JOIN
+            `tabHas Role` r ON u.name = r.parent
+        WHERE
+            r.role = 'Member' AND u.enabled = 1 AND u.user_type = 'Website User'
+    """, as_dict=True)
+
+    return users
+
