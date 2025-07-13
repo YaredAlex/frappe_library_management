ade1192aa494a1779b03e5ecfe1550e652d205e9
YaredAlex <yareda25@gmail.com>
2025-07-13 23:18:15 +0300

    added phone

--- library_management/api/user.py
+++ library_management/api/user.py
@@ -2,7 +2,7 @@ import frappe
 from frappe import _
 
 @frappe.whitelist()
-def create_user(email, first_name, last_name, role, password):
+def create_user(email, first_name, last_name, role, password,phone):
     """
     Creates a new user with the specified role and securely sets the password.
     """
@@ -15,11 +15,25 @@ def create_user(email, first_name, last_name, role, password):
         "first_name": first_name,
         "last_name": last_name,
         "enabled": 1,
+        "phone":phone,
         "new_password": password,
         "send_welcome_email": 0,
         "roles": [{"role": role}]
     })
     user.insert(ignore_permissions=True)
 
-    return email
-    
+    return user
+
+@frappe.whitelist()   
+def user_details(email):
+    if not frappe.db.exists("User", email):
+        frappe.throw("User not found")
+
+    user = frappe.get_doc("User", email)
+    return {
+        "email": user.email,
+        "first_name": user.first_name,
+        "last_name": user.last_name,
+        "username":user.username,
+        "roles": [r.role for r in user.roles]
+    }
